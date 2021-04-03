7f2001d692b26e261ed94081e9dac076070667f5
Convicts <convicts@jhon_mac.home>
2021-04-03 17:02:00 +0200

    [add] libft

--- libft/libft.h
+++ libft/libft.h
@@ -21,4 +21,6 @@ int		ft_strncmp(char *s1, char *s2, size_t n);
 int		ft_isdigit(int c);
 char	*ft_strjoin(char *s1, char *s2);
 void	*ft_calloc(size_t count, size_t size);
+void	*ft_memcpy(void *dst, void *src, size_t n);
+void	*ft_realloc(void *ptr, size_t size);
 #endif
