bd37948d11983a2c11b2c62f3e3af753d84c3183
Jonathan DE-VRIEZE <jode-vri@e1r9p4.42.fr>
2021-04-06 16:32:11 +0200

    [current] merge.c

--- src/merge.c
+++ src/merge.c
@@ -4,17 +4,24 @@ void	get_cmds(t_minishell *ms)
 {
 	char	**cmds;
 	int		i;
+	int		j;
+	int		k;
 	int		len;
 
-	cmds = ft_split(ms->line, ";");
+	cmds = ft_split((char *)ms->line, ";");
 	len = ft_strs_tab_size(cmds);
-	ms->cmds = malloc(sizeof(char *) * len);
+	ms->cmds = malloc(sizeof(char **) * len);
 	i = 0;
+	k = 0;
 	while (i < len)
 	{
-		ms->cmds[i] = ft_strtrim(cmds[i], " ");
-		printf("|%s|\n", ms->cmds[i]);
+		j = 0;
+		while (cmds[i][j])
+			j++;
+		ms->cmds[i] = malloc(sizeof(char *) * (j + 1));
+		ms->cmds[i] = ft_split(cmds[i], " ");
 		i++;
+		k++;
 	}
 }
 
