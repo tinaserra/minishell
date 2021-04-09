784b6adddd25c964adc38fd6afb6d8b9501737d7
jode-vri <jode-vri@student.42.fr>
2021-04-09 15:10:33 +0200

    [add] echo builtin

--- src/john.c
+++ src/john.c
@@ -2,27 +2,7 @@
 
 // void	get_cmds(t_minishell *ms)
 // {
-// 	char	**cmds;
-// 	int		i;
-// 	int		j;
-// 	int		k;
-// 	int		len;
-
-// 	cmds = ft_split((char *)ms->line, ";");
-// 	len = ft_strs_tab_size(cmds);
-// 	ms->cmds = malloc(sizeof(char **) * len);
-// 	i = 0;
-// 	k = 0;
-// 	while (i < len)
-// 	{
-// 		j = 0;
-// 		while (cmds[i][j])
-// 			j++;
-// 		ms->cmds[i] = malloc(sizeof(char *) * (j + 1));
-// 		ms->cmds[i] = ft_split(cmds[i], " ");
-// 		i++;
-// 		k++;
-// 	}
+	
 // }
 
 	/*pid_t	pid;
@@ -45,55 +25,35 @@
 			waitpid(pid, &status, 0);
 	}*/
 
-// void	echo_builtin(t_minishell *ms, int i)
-// {
-// 	int 	j;
-// 	int		n;
-// 	int 	k;
-// 	char	*path_var;
-// 	char	*res;
-
-// 	j = 0;
-// 	n = 0;
-// 	if (ms->cmds[i][1] == NULL)
-// 		ft_putstr_fd(1, "\n");
-// 	if (ft_strcmp(ms->cmds[i][1], "-n") == 0)
-// 	{
-// 		j++;
-// 		n = 1;
-// 	}
-// 	while (ms->cmds[i][++j])
-// 	{		
-// 		k = -1;
-// 		while (ms->cmds[i][j][++k])
-// 		{
-// 			if (ms->cmds[i][j][k] == '$')
-// 			{
-// 				path_var = ft_strtrim(ms->cmds[i][j], "$");
-// 				res = getenv(path_var);
-// 				ft_putstr_fd(1, res);
-// 				while (ms->cmds[i][j][k] && ms->cmds[i][j][k] != ' ')
-// 					k++;
-// 			}
-// 			else
-// 				ft_putchar_fd(ms->cmds[i][j][k], 1);
-// 		}
-// 		if (ms->cmds[i][j + 1] != NULL)
-// 			ft_putstr_fd(1, " ");
-// 		else if (ms->cmds[i][j + 1] == NULL && !n)
-// 			ft_putstr_fd(1, "\n");
-// 	}
-// }
-
-void fonction()
+static void	execute(t_cmds *cmds)
 {
-	int	i;
+	if (ft_strcmp(cmds->cmd, "echo") == 0)
+		echo_builtin(cmds);
+}
 
+void 		fonction()
+{
+	char	**cmdss;
+	t_cmds	cmds;
+	int		i;
+	int		j;
+
+	cmds.cmds_list = malloc(sizeof(t_list));
+	ft_bzero(&cmds, sizeof(t_cmds));
+	cmdss = ft_split((char *)ms->line, ";");
+	free(ms->line);
 	i = -1;
-	// get_cmds(&ms);
-	// while (ms.cmds[++i])
-	// {
-	// 	if (ft_strcmp(ms.cmds[i][0], "echo") == 0)
-	// 		echo_builtin(&ms, i);
-	// }
+	while (cmdss[++i])
+	{
+		cmds.args = ft_split(cmdss[i], "\f\t\n\r\v ");
+		cmds.cmd = cmds.args[0];
+		j = 0;
+		while (cmds.args[++j])
+			ft_lstadd_back(&cmds.cmds_list, ft_lstnew(cmds.args[j]));
+		execute(&cmds);
+		free(cmds.cmd);
+		//TODO: free cmds.cmds_list
+		cmds.cmds_list = NULL;
+	}
+	ft_free_tab(cmdss);
 }
