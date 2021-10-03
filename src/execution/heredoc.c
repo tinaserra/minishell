be733e686ea824d7ccfbadbc7d22e39c26e80cb6
jode-vri <jode-vri@student.42.fr>
2021-10-03 15:11:18 +0000

    heredoc ok

--- src/execution/heredoc.c
+++ src/execution/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: jode-vri <jode-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 06:51:46 by jode-vri          #+#    #+#             */
-/*   Updated: 2021/09/27 07:57:48 by jode-vri         ###   ########.fr       */
+/*   Updated: 2021/10/03 15:11:01 by jode-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ void	heredoc(t_cmd *cmd, t_token **token)
 	char	*line;
 	char	*tmp;
 	int		fd[2];
-	//int		tmp1;
 
 	pipe(fd);
 	cmd->in = fd[0];
-	//tmp1 = 0;
 	tmp = NULL;
 	line = ft_strdup("");
 	(*token)->next->word = handle_quotes((*token)->next->word, 0);
@@ -32,13 +30,13 @@ void	heredoc(t_cmd *cmd, t_token **token)
 		write(2, "> ", 2);
 		if (ft_get_next_line(0, &tmp) && ft_strcmp((*token)->next->word, tmp) != 0)
 		{
+			if (is_in_str(tmp, '$'))
+				tmp = replace_env(tmp, ft_find_in_str(tmp, '$'));
 			line = ft_strjoin_free(line, tmp, 'L');
 			line = ft_strjoin_free(line, "\n", 'L');
 		}
 	}
 	free(tmp);
-	//if (is_in_str(line, '$'))
-	//	line = replace_env2(line, 0);
 	ft_putstr_fd(line, fd[1]);
 	free(line);
 	close(fd[1]);
