d0c7ebb1ea79a0445bb763ba2de7871328d0b7eb
jode-vri <jode-vri@student.42.fr>
2021-11-26 17:52:58 +0000

    norme

--- src/execution/exec_start.c
+++ src/execution/exec_start.c
@@ -91,49 +91,6 @@ static void	exec_switch(t_cmd *cmd, int pipe1[2])
 		exec_binary(cmd, pipe1, NULL);
 }
 
-t_token	*lstcreate(char *data, t_token *next)
-{
-	t_token	*cell;
-
-	cell = malloc(sizeof(t_token));
-	if (cell == NULL)
-		return (NULL);
-	cell->word = ft_strdup(data);
-	cell->type = 5;
-	cell->next = next;
-	return (cell);
-}
-
-void test(t_cmd *cmd)
-{
-	char	**res;
-	t_token	*tmp;
-	t_token *tmp2;
-	int		i;
-
-	if (is_in_str(cmd->cmd, ' '))
-	{
-		i = 0;
-		res = ft_split(cmd->cmd, " ");
-		free(cmd->cmd);
-		cmd->cmd = ft_strdup(res[0]);
-		tmp = cmd->args->next;
-		while (cmd->args)
-		{
-			tmp2 = cmd->args->next;
-			free(cmd->args->word);
-			free(cmd->args);
-			cmd->args = tmp2;
-		}
-		cmd->args = lstcreate(res[1], tmp);
-		free(cmd->bin);
-		free(cmd->cmd);
-		cmd->cmd = ft_strdup(res[0]);
-		ft_free_tab(res);
-	}
-	g_ms->double_chk = 1;
-}
-
 void	exec_start(void)
 {
 	t_cmd	*cmd;
