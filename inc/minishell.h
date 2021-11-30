79f8a0727f66ee84f43b63f2995da2287d6a48b5
jode-vri <jode-vri@student.42.fr>
2021-11-30 13:48:19 +0000

    env

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jode-vri <jode-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 14:50:05 by admin             #+#    #+#             */
-/*   Updated: 2021/11/30 13:33:12 by jode-vri         ###   ########.fr       */
+/*   Updated: 2021/11/30 13:39:47 by jode-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,7 @@ t_token	*redirect3(t_token *args, t_token **start);
 */
 
 void	echo_builtin(t_cmd *cmd, int fd);
-void	env_builtin(int fd);
+void	env_builtin(t_token *args, int fd);
 void	cd_builtin(t_cmd *cmd);
 void	pwd_builtin(t_cmd *cmd);
 void	unset_builtin(t_cmd *cmd);
