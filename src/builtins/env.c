eaa8611e89268c9783e024094367d1a3f2ef71d9
jode-vri <jode-vri@student.42.fr>
2021-11-30 13:48:00 +0000

    env no args

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: jode-vri <jode-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 04:08:16 by jode-vri          #+#    #+#             */
-/*   Updated: 2021/11/30 13:34:57 by jode-vri         ###   ########.fr       */
+/*   Updated: 2021/11/30 13:44:09 by jode-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,18 @@ void	export_x(int fd)
 	}
 }
 
-void	env_builtin(int fd)
+void	env_builtin(t_token *args, int fd)
 {
 	t_env	*tmp;
 
 	tmp = g_ms->env;
 	if (fd == 0)
 		fd = 1;
+	if (args)
+	{
+		error("No such file or directory", "env", args->word, 127);
+		return ;
+	}
 	while (tmp)
 	{
 		ft_putstr_fd(tmp->name, fd);
